ef9080eaa117cc50a11c1007439e0e8f97286084
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-24 12:59:26 +0300

    components and pages continue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
 import './App.css';
+import Nav from "./components/Nav"
+import Home from "./pages/Home"
+import People from "./pages/People"
+import Contact from "./pages/Contact"
+import NotFound from "./pages/NotFound"
+import Footer from "./components/Footer"
+import Paths from "./pages/Paths"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/people" element={<People/>} />
+        <Route path="/paths" element={<Paths/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="*" element={<NotFound/>} />          
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
-
 export default App;
